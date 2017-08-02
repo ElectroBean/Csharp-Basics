58a19f66e533f7900a043c364034caaa1cb09540
Deeon Burgan <dburgan@live.com>
2017-08-02 12:00:23 +1000

    Initial Commit for C# basics

---
+++ csharp/csharp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace csharp
+{
+    class Program
+    {
+        enum choices{
+            Add = '+', 
+            Subtract = '-',
+            Multiply = '*', 
+            Divide = '/',
+        }
+
+
+        static void Main(string[] args)
+        {
+            firstClass myClass = new firstClass();
+            //Console.Write("What's ur name?");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("hi, " + name);
+            Console.Write("What you wanna do?");
+            //Console.WriteLine();
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "asd":
+                    Console.WriteLine("asdddedasdfasdfasdf");
+
+
+
+                    break;
+                case "anything else":
+                    Console.WriteLine("AnythingBlahhh");
+
+
+
+                    break;
+
+                default:
+                    Console.WriteLine("Nothing matched the case");
+
+                    break;
+
+            }
+            
+            while (true) ;
+        }
+        
+    }
+}
---
+++ csharp/csharp/firstClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    class firstClass
+    {
+        public float Addition(float n1, float n2)
+        {
+            return (n1 + n2);
+        }
+
+        public float Division(float n1, float n2)
+        {
+            return (n1 / n2);
+        }
+
+        public float Multiplication(float n1, float n2)
+        {
+            return n1 * n2;
+        }
+
+        public float Subtraction(float n1 , float n2)
+        {
+            return n1 - n2;
+        }
+    }
+}
