0ad8b83760b45ed80c9e09630e2a57d92f3c30e0
Deeon Burgan <dburgan@live.com>
2017-08-07 16:13:27 +1000

    Arrays exercises

--- csharp/csharp/Program.cs
+++ csharp/csharp/Program.cs
@@ -9,47 +9,93 @@ namespace csharp
 {
     class Program
     {
-        enum choices{
-            Add = '+', 
+        static void ArrayExample(int size)
+        {
+            int[] array = new int[size];
+            foreach (int index in array)
+            {
+                //index = 1;  cant change variable in foreach loop
+                Console.WriteLine(array[index]);
+            }
+        }
+
+        static int[] array = new int[69];
+        static void ArrayExample2(int[] array)
+        {
+            Array ar = array;
+            int length = ar.Length;
+            Console.WriteLine(length);
+        }
+
+        enum choices
+        {
+            Add = '+',
             Subtract = '-',
-            Multiply = '*', 
+            Multiply = '*',
             Divide = '/',
         }
 
 
         static void Main(string[] args)
         {
-            firstClass myClass = new firstClass();
-            //Console.Write("What's ur name?");
-            //string name = Console.ReadLine();
-            //Console.WriteLine("hi, " + name);
-            Console.Write("What you wanna do?");
-            //Console.WriteLine();
-            string choice = Console.ReadLine();
-            switch (choice)
-            {
-                case "asd":
-                    Console.WriteLine("asdddedasdfasdfasdf");
+            //firstClass myClass = new firstClass();
+            //Console.Write("What you wanna do?");
+            //string choice = Console.ReadLine();
+            //float a = 0;
+            //float b = 0;
+            //float c = 0;
+            //
+            //switch (choice)
+            //{
+            //    case "+":
+            //        Console.Write("Enter 2 numbers");
+            //        Console.WriteLine("");
+            //        a = Convert.ToSingle(Console.ReadLine());
+            //        b = Convert.ToSingle(Console.ReadLine());
+            //        c = myClass.Addition(a, b);
+            //        Console.WriteLine(a + " plus " + b + " is " + c);
+            //        break;
+            //    case "-":
+            //        Console.Write("Enter 2 numbers");
+            //        Console.WriteLine("");
+            //        a = Convert.ToSingle(Console.ReadLine());
+            //        b = Convert.ToSingle(Console.ReadLine());
+            //        c = myClass.Subtraction(a, b);
+            //        Console.WriteLine(a + " minus " + b + " is " + c);
+            //        break;
+            //
+            //    case "/":
+            //        Console.Write("Enter 2 numbers");
+            //        Console.WriteLine("");
+            //        a = Convert.ToSingle(Console.ReadLine());
+            //        b = Convert.ToSingle(Console.ReadLine());
+            //        c = myClass.Division(a, b);
+            //        Console.WriteLine(a + " divided by " + b + " is " + c);
+            //        break;
+            //
+            //    case "*":
+            //        Console.Write("Enter 2 numbers");
+            //        Console.WriteLine("");
+            //        a = Convert.ToSingle(Console.ReadLine());
+            //        b = Convert.ToSingle(Console.ReadLine());
+            //        c = myClass.Multiplication(a, b);
+            //        Console.WriteLine(a + " multiplied by " + b + " is " + c);
+            //        break;
+            //
+            //    default:
+            //        Console.WriteLine("That's not a function");
+            //
+            //        break;
+            //
+            //}
 
-
-
-                    break;
-                case "anything else":
-                    Console.WriteLine("AnythingBlahhh");
-
-
-
-                    break;
-
-                default:
-                    Console.WriteLine("Nothing matched the case");
-
-                    break;
-
-            }
-            
+            //ArrayExample(10);
+            ArrayExample2(array);
+            Console.WriteLine(array[100]);
             while (true) ;
         }
-        
+
     }
+
+
 }
